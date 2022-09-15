8c6323e4be6788103d4ed31f7f0c329a88b405f6
amitv9493 <amitv9493@gmail.com>
2022-09-15 09:42:18 -0800

    add blog app

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display= ['title', 'slug', 'author', 'publish', 'status']
+
+    list_filter = ['status', 'created', 'publish', 'author']
+    prepopulated_fields= {'slug': ('title',)}
+    row_id_fields = ['author']
+    date_hierarchy= 'publish'
+    ordering= ['status', 'publish']
+    search_fields = ['title', 'body']
+
+
---
+++ myproject/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from blog import views
+
+urlpatterns = [
+    path('',views.post_list, name='post_list'),
+    path('<int:id>', views.post_detail, name = 'post_detail'),
+    
+]
