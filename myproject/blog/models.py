a2f6a4171b845d45212a6d01ce9bd3603adfc474
amitv9493 <amitv9493@gmail.com>
2022-09-15 04:41:34 -0800

    update models.py with posts

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,3 +1,28 @@
+from random import choices
+from time import timezone
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Post(models.Model):
+    # MAKING CHOICES WITH THIS Class
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add= True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+    class meta:
+        ordering = ['-publish']
+        indexes = [models.Index(fields=['-publish'])]
+
+    def __str__(self):
+        return self.title
