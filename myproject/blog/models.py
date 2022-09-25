634eaf0e0719887e35946078db544d2079da47cb
amitv9493 <amitv9493@gmail.com>
2022-09-26 00:55:50 +0530

    updated some features

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -35,6 +35,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 "Creating custom Manager"
 
@@ -49,7 +50,7 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
@@ -70,3 +71,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug,
+
+
+
+        ])
+    
--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from blog import views
-
+app_name = 'blog'
 urlpatterns = [
     path('',views.post_list, name='post_list'),
-    path('<int:id>', views.post_detail, name = 'post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name = 'post_detail'),
     
 ]
